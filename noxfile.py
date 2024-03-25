8fd0f2fb10486f95407fb392c76cdebab04c74a2
marksie1988 <marksie1988@noreply.github.com>
2024-03-25 16:55:48 +0000

    build: fix build issues

--- noxfile.py
+++ noxfile.py
@@ -46,7 +46,5 @@ def docs(session: Session) -> None:
 @nox.session(reuse_venv=True)
 def release(session: Session) -> None:
     """Release a new version of the package"""
-    pypi_password = session.posargs[0]
     session.run("poetry", "install", external=True)
     session.run("npx", "semantic-release", "--debug")
-    session.run("poetry", "publish", "-u", "__token__", "-p", pypi_password)
